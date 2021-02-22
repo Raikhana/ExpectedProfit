eff0b76a2b398a773f45d060bb9957897a16676a
Raikhana <raikhanaurazbaeva@gmail.com>
2021-02-21 17:55:09 -0700

    This program calculates Expected Profit. Personal project

---
+++ ExpectedPrice/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ExpectedPrice
+{
+    partial class frmReturnOnInvestment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtInvestment = new System.Windows.Forms.TextBox();
+            this.txtInterestROI = new System.Windows.Forms.TextBox();
+            this.txtInvestmentGain = new System.Windows.Forms.TextBox();
+            this.txtProfit = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Investment";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ROI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Investment gain";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Profit";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(80, 123);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(165, 123);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(45, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtInvestment
+            // 
+            this.txtInvestment.Location = new System.Drawing.Point(110, 15);
+            this.txtInvestment.Name = "txtInvestment";
+            this.txtInvestment.Size = new System.Drawing.Size(100, 20);
+            this.txtInvestment.TabIndex = 6;
+            // 
+            // txtInterestROI
+            // 
+            this.txtInterestROI.Location = new System.Drawing.Point(110, 41);
+            this.txtInterestROI.Name = "txtInterestROI";
+            this.txtInterestROI.Size = new System.Drawing.Size(100, 20);
+            this.txtInterestROI.TabIndex = 7;
+            // 
+            // txtInvestmentGain
+            // 
+            this.txtInvestmentGain.Location = new System.Drawing.Point(110, 67);
+            this.txtInvestmentGain.Name = "txtInvestmentGain";
+            this.txtInvestmentGain.ReadOnly = true;
+            this.txtInvestmentGain.Size = new System.Drawing.Size(100, 20);
+            this.txtInvestmentGain.TabIndex = 8;
+            // 
+            // txtProfit
+            // 
+            this.txtProfit.Location = new System.Drawing.Point(110, 93);
+            this.txtProfit.Name = "txtProfit";
+            this.txtProfit.ReadOnly = true;
+            this.txtProfit.Size = new System.Drawing.Size(100, 20);
+            this.txtProfit.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "* ROI - Return on investment";
+            // 
+            // frmReturnOnInvestment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 210);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtProfit);
+            this.Controls.Add(this.txtInvestmentGain);
+            this.Controls.Add(this.txtInterestROI);
+            this.Controls.Add(this.txtInvestment);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmReturnOnInvestment";
+            this.Text = "Return on investment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtInvestment;
+        private System.Windows.Forms.TextBox txtInterestROI;
+        private System.Windows.Forms.TextBox txtInvestmentGain;
+        private System.Windows.Forms.TextBox txtProfit;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ExpectedPrice/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpectedPrice
+{
+    public partial class frmReturnOnInvestment : Form
+    {
+        public frmReturnOnInvestment()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal investment = Convert.ToDecimal(txtInvestment.Text);
+            decimal ROI = Convert.ToDecimal(txtInterestROI.Text);
+
+            decimal investmentGain = (ROI / 100) * investment + investment;
+            decimal profit = investmentGain - investment;
+
+            txtInvestment.Text = investment.ToString("c");
+            txtInterestROI.Text = ROI.ToString() + "%";
+            txtInvestmentGain.Text = investmentGain.ToString("c");
+            txtProfit.Text = profit.ToString("c");
+
+            txtInvestment.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
